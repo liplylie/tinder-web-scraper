50fe3498494213eb8c8b86b93b28a9c9880b7355
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-11-28 23:49:15 -0800

    add full login functionality

--- index.js
+++ index.js
@@ -13,12 +13,24 @@ var driver = new webdriver.Builder()
 
 driver.get('http://www.facebook.com/');
 driver.findElement(By.name('email')).sendKeys(secret.email);
-driver.findElement(By.name('pass')).sendKeys(secret.password);
-driver.wait(until.titleIs('Facebook - Log In or Sign Up'), 10000);
-driver.findElement(By.id('u_0_2')).click();
-driver.wait(until.titleIs('Facebook'), 10000);
-driver.get('http://www.tinder.com/');
-driver.wait(until.titleIs('Tinder | Swipe. Match. Chat.'), 50000);
-driver.findElement(By.linkText('Log in with Facebook')).click();
+driver.findElement(By.name('pass')).sendKeys(secret.password)
+  .then(driver.findElement(By.id('loginbutton')).click())
 
-driver.quit();
+driver.get('http://www.tinder.com/')
+  .then(driver.wait(until.elementLocated(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div[1]/div/div[3]/button[1]')), 10000))
+    .then(driver.findElement(By.xpath('//*[@id="modal-manager"]/div/div/div[2]/div[1]/div/div[3]/button[1]')).click())
+      .then(driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div[1]/div[1]/div/button')), 10000))
+        .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div[1]/div[1]/div/button')).click())
+          .then(driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/main/div/button')), 10000))
+            .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/main/div/button')).click())
+              .then(driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]')), 10000))
+                .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]')).click())
+                  .then(driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]')), 10000))
+                    .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]')).click());
+
+  
+                
+
+    
+
+// driver.quit();
