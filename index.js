36b3346a5cd26f267a151be4fffac008a925d02b
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-11-29 17:45:20 -0800

    add experimental setting to disable chrome notifications

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const path = require('path');
 const parser = require('body-parser');
 const secret = require('./secret.json');
 
-var webdriver = require('selenium-webdriver'),
+let webdriver = require('selenium-webdriver'),
 By = webdriver.By,
 until = webdriver.until;
 
-var driver = new webdriver.Builder()
-  .forBrowser('chrome')
-    .build();
+// let driver = new webdriver.Builder()
+//   .forBrowser('chrome')
+//     .build();
+
+
+let chromeCapabilities = webdriver.Capabilities.chrome();
+//setting chrome options to start the browser fully maximized
+let chromeOptions = {
+    'args': ['--disable-notifications']
+};
+chromeCapabilities.set('chromeOptions', chromeOptions);
+let driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
 
 driver.get('http://www.facebook.com/');
 driver.findElement(By.name('email')).sendKeys(secret.email);
@@ -25,20 +34,17 @@ driver.get('http://www.tinder.com/')
             .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/main/div/button')).click())
               .then(driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]')), 10000))
                 .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]')).click())
-                .then(driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')), 10000))
-                .then(driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')), 10000))
-                .then(driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')), 10000))
-                .then(driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')), 10000))
-                  .then(driver.findElement(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')).click())
-                    .then(() => { 
-                      /*adjust iterations, how many profiles you'd like to swipe. it's set to 1000 by default*/
-                      for (var i = 0; i < 1000; i++) {
-                        driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[5]')), 10000)
-                        /*currently, the app swipes left. uncomment this line to swipe right and comment out the line after it to swipe left*/
-                        // .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[4]')).click())
-                        .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[2]')).click())
-                          .then(driver.actions().sendKeys(webdriver.Key.ESCAPE).perform());
-                      }
-                    });
+                  .then(driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')), 10000))
+                    .then(driver.findElement(By.xpath('//div[contains(@class, "onboarding__buttons Bgc(#fff)--s")][button][1]')).click())
+                      .then(() => { 
+                        /*adjust iterations, how many profiles you'd like to swipe. it's set to 1000 by default*/
+                        for (var i = 0; i < 1000; i++) {
+                          driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[5]')), 10000)
+                          /*currently, the app swipes left. uncomment this line to swipe right and comment out the line after it to swipe left*/
+                          // .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[4]')).click())
+                          .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[2]')).click())
+                            .then(driver.actions().sendKeys(webdriver.Key.ESCAPE).perform());
+                        }
+                      });
 
 // driver.quit();
