00d14b37a96d87011c9ff31fe77b43df0ca00cd1
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-11-30 18:52:13 -0800

    add ability to extract links from open cards

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
 const secret = require('./secret.json');
+const promise = require('selenium-webdriver').promise;
 
 let webdriver = require('selenium-webdriver'),
 By = webdriver.By,
@@ -27,28 +28,35 @@ driver.findElement(By.name('email')).sendKeys(secret.email)
               //start swiping
               .then(() => { 
                 /* adjust iterations, how many profiles you'd like to swipe. it's set to 100 by default */
-                for (var i = 0; i < 100; i++) {
+                for (var i = 0; i < 2; i++) {
                   driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[5]')), 20000)
                     // .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[4]')).click())
                     /* line above this comment swipes right. line below swipes left. Comment/uncomment them per direction you'd like to swipe */
+
+                    .then(driver.actions().sendKeys(webdriver.Key.ARROW_UP).perform())
+                    .then(driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div')), 20000))
                     .then(() => {
-                      let image = driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[1]'))
-                      image.getCssValue('background-image')
-                        .then(val => {
-                          val = val.substring(5, val.length - 2)
-                          console.log(val, 'val')
-                          //driver.get(val)
-                        })
-                      //driver.get(image)
-                      //
-                      // for other's pic:
-                      // driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[1]')).getCssValue("style")
-                      // for your own pic:
-                      //driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[1]/img')).getAttribute("src")
-                    })
-                    .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[2]')).click())
-                      .then(driver.actions().sendKeys(webdriver.Key.ESCAPE).perform());
-                  console.log(i + ' profiles swiped');
+                        driver.findElements(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div[1]/a/div/div[1]/div/div'))                    
+                          .then((elements) => {
+                            console.log('there are ', elements.length, ' images for this user');
+                              elements[0].findElements(By.css('.profileCard__slider__imgHelper'))
+                                .then((elements) => {
+                                  elements.forEach((element) => {
+                                    element.findElements(By.css('.profileCard__slider__img'))
+                                      .then((elements) => {
+                                         elements[0].getAttribute('src')
+                                           .then((src) => {
+                                             console.log('src is: ', src);
+                                           });
+                                      });
+                                  })
+                                });
+                            });
+                      })
+                        .then(driver.actions().sendKeys(webdriver.Key.ARROW_DOWN).perform())
+                            .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[2]')).click())
+                              .then(driver.actions().sendKeys(webdriver.Key.ESCAPE).perform());
+                              console.log(i + ' profiles swiped');
                 }
               });
 
