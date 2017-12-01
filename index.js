25d869654285c0830702a8d632dda2b01b6d1f86
Jordan Daniels <decentpianist@gmail.com>
2017-11-30 22:57:20 -0800

    get faceId from azure face detect api

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const axios = require('axios')
 const path = require('path');
 const parser = require('body-parser');
 const secret = require('./secret.json');
-const domainName = 'http://localhost:3000'
+const domainName = 'http://localhost:3000';
+
+let dummyFace = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzQzMTY2NDc4/will-smith-9542165-1-402.jpg"
 let urls = [];
+let faceIds = [];
 let webdriver = require('selenium-webdriver'),
 By = webdriver.By,
 until = webdriver.until;
@@ -42,7 +45,6 @@ driver.findElement(By.name('email')).sendKeys(secret.email)
                           if (val.length > 5){
                             urls.push(val)
                           }
-                          
                           //driver.get(val)
                         })
                       //driver.get(image)
@@ -55,24 +57,62 @@ driver.findElement(By.name('email')).sendKeys(secret.email)
                     .then(driver.findElement(By.xpath('//*[@id="content"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[2]/button[2]')).click())
                       .then(driver.actions().sendKeys(webdriver.Key.ESCAPE).perform());
                 }
-              }).then(()=>{
-                  // console.log(urls,'urls')
-                  for ( let j = 0; j < urls.length; j++){
-                    axios.post(`${domainName}/api/postUrl`, {
+              })
+              .then(()=>{
+                // console.log(urls,'urls')
+                // stores urls into database
+                for ( let j = 0; j < urls.length; j++) {
+                  axios.post(`${domainName}/api/postUrl`, {
                     url: urls[j]
-                      })
-                      .then(()=> {
-                        console.log('post sent')
-                      })
-                      .catch((err)=> {
-                        console.log(err, 'post failed')
-                      })
+                    })
+                    .then(data =>{
+                      console.log('post sent', data)
+                    })
+                    .catch(err => {
+                      console.log(err, 'post failed')
+                    })
                   }
-                  
                 })
-              
-
-
-module.exports = urls
+                .then(()=>{
+                  // face detect - get face id
+                  let dataDetect = {
+                    "url": dummyFace,
+                    "returnFaceId": "true",
+                    "returnFaceLandmarks": "false"
+                  }
+                  axios.post("https://westus.api.cognitive.microsoft.com/face/v1.0/detect?", dataDetect, {
+                    headers: {
+                      "Content-Type": "application/json",
+                      "Ocp-Apim-Subscription-Key": secret.Key1
+                    }
+                  })
+                  .then(function({data}) {
+                      console.log("data from detect", data);
+                      faceIds.push(data)
+                      console.log(faceIds, 'faceIds')
+                  })
+                  .catch(function(err) {
+                      console.log("error from detect", err);
+                  });
+                })
+                // .then(()=>{
+                //   // face verify - take two faces and compare them
+                //   let dataVerify = {
+                //     faceId1: "",
+                //     faceId2: "",
+                //   }
+                //   axios.post("https://westus.api.cognitive.microsoft.com/face/v1.0/verify", dataVerify, {
+                //     headers: {
+                //       "Content-Type": "application/json",
+                //       "Ocp-Apim-Subscription-Key": secret.key1
+                //     }
+                //   })
+                //   .then(data => {
+                //       console.log("data from verify", data);
+                //   })
+                //   .error(err => {
+                //       console.log(err,"error from verify");
+                //   })
+                // })
 
 // driver.quit();
